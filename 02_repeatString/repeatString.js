7d873330b02aa95adc699968ad8d0b63e6f8bd38
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-29 19:26:02 +0800

    Completed the repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (inputString, repeatNum) {
+  let input = inputString;
 
+  for (let i = 1; i < repeatNum; i++) {
+    input += inputString;
+  }
+
+  if (repeatNum == 0) {
+    return ``;
+  } else if (repeatNum < 0) {
+    return `ERROR`;
+  } else {
+    return input;
+  }
 };
 
 // Do not edit below this line
