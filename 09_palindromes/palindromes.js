4592d9c7756e98792447910c1f42fe11d4ef2d28
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-30 16:00:34 +0800

    Finished the palindrome exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (palindromeInput) {
+  let isPalindrome;
+  const palindrome = palindromeInput
+    .replace(/\s/g, ``)
+    .replace(/\W/g, ``)
+    .toLowerCase();
+  let j = palindrome.length - 1;
 
+  for (let i = 0; i < palindrome.length / 2; i++) {
+    if (palindrome[i] == palindrome[j]) {
+      isPalindrome = true;
+    } else {
+      isPalindrome = false;
+      break;
+    }
+    j--;
+  }
+
+  return isPalindrome;
 };
 
 // Do not edit below this line
