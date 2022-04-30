a76aeae328fc21888852858da67b7c2982c3c204
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-30 14:15:16 +0800

    Finished the sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (startNum, lastNum) {
+  let sum = 0;
+  const startNumber = startNum;
+  const lastNumber = lastNum;
+  const firstError = startNumber < 0 || lastNumber < 0;
+  const secondError = typeof startNumber && typeof lastNumber != `number`;
 
+  if (firstError || secondError) {
+    return `ERROR`;
+  } else {
+    if (startNumber < lastNumber) {
+      for (let i = startNumber; i <= lastNumber; i++) {
+        sum += i;
+      }
+    } else {
+      for (let i = startNumber; i >= lastNumber; i--) {
+        sum += i;
+      }
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
