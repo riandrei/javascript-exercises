c00fa14656440898041d2f9b9a29bb4b01fb0c31
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-30 16:28:45 +0800

    Finished the getTheTitles exercise.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (objectArray) {
+  const titleArray = [];
+  for (let i = 0; i < objectArray.length; i++) {
+    titleArray.push(objectArray[i].title);
+  }
+  return titleArray;
 };
 
 // Do not edit below this line
