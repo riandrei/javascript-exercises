bbef4976ff017203e289bc8d795f5ed7311ee129
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-30 13:27:39 +0800

    Finished the removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  const array = arguments[0];
 
+  for (let i = 1; i < arguments.length; i++) {
+    let filterValues = array.indexOf(arguments[i]);
+    if (filterValues >= 0) {
+      array.splice(filterValues, 1);
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
