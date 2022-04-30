c7be4ca7d9fba8ba3c7993875435c17457e5bbb5
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-04-30 16:21:15 +0800

    Finished the fibonacci exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (nthFibonacci) {
+  const array = [0, 1];
+  if (nthFibonacci < 0) {
+    return `OOPS`;
+  }
+  for (let i = 2; i <= nthFibonacci + 2; i++) {
+    array[i] = array[i - 2] + array[i - 1];
+  }
+  return array[nthFibonacci];
 };
 
 // Do not edit below this line
