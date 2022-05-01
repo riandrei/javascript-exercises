0d0e7b635452cc4bdfd5ff9f58d13d7c46bb5903
Riandrei Casañas <riandrei.casanas@gmail.com>
2022-05-01 13:14:59 +0800

    Finished the findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (objArray) {
+  return objArray.reduce(getOldest);
 };
+
+function getOldest(oldestPerson, currentPerson) {
+  let currentYear = new Date();
+  let oldestPersonAge = 0;
+  let currentPersonAge = 0;
+
+  if (oldestPerson.yearOfDeath === undefined) {
+    oldestPersonAge = currentYear.getFullYear() - oldestPerson.yearOfBirth;
+  } else {
+    oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+  }
+  if (currentPerson.yearOfDeath === undefined) {
+    currentPersonAge = currentYear.getFullYear() - currentPerson.yearOfBirth;
+  } else {
+    currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+  }
+
+  if (oldestPersonAge < currentPersonAge) {
+    return (oldestPerson = currentPerson);
+  } else {
+    return oldestPerson;
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
